ef2585eea1c836fb1f47281ebeeb7208d1c967af
Edunno <tamericandream@hotmail.com>
2019-05-01 10:30:11 +0200

    Update LocationsTimeDTO.java
    LocationsTimeDTO

--- src/main/java/dto/LocationsTimeDTO.java
+++ src/main/java/dto/LocationsTimeDTO.java
@@ -2,10 +2,35 @@
 
 package dto;
 
+import entity.LocationsTime;
+import java.util.Date;
+
 /**
  * @author Esben
  * All rights belong to respective contributors.
  */
 public class LocationsTimeDTO {
 
+    int id;
+    String name;
+    String status;
+    Date startsAt;
+    Date endsAt;
+    int longitude;
+    int latitude;
+    int carId;
+    
+    public LocationsTimeDTO(LocationsTime a){
+        this.id = a.getId();
+        this.name = a.getName();
+        this.status = a.getStatus();
+        this.startsAt = a.getStartsAt();
+        this.endsAt = a.getEndsAt();
+        this.longitude = a.getLocLongitude();
+        this.latitude = a.getLocLatitude();
+        this.carId = a.getCarId().getId();
+    }
+    
+    
+    
 }

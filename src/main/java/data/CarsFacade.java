0227585c3bfc426748c1b3cdef131a21bd03cc07
Edunno <tamericandream@hotmail.com>
2019-05-01 13:10:28 +0200

    Update CarsFacade.java
    CarsFacedeAddition

--- src/main/java/data/CarsFacade.java
+++ src/main/java/data/CarsFacade.java
@@ -1,44 +1,59 @@
 /* Esben; DECK-CS */
-
 package data;
+
 import entity.Cars;
 import java.util.Collection;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+
 /**
- * @author Esben
- * All rights belong to respective contributors.
+ * @author Esben All rights belong to respective contributors.
  */
 public class CarsFacade {
-        
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-        
-    public Collection<Cars> getAllCars(){
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+
+    public void addEntityManager(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public Collection<Cars> getAllCars() {
         EntityManager em = emf.createEntityManager();
         Query q = em.createNamedQuery("Cars.findAll");
         Collection<Cars> carList = q.getResultList();
         return carList;
-    } 
-    public Cars getCarById(int id){
+    }
+
+    public Cars getCarById(int id) {
         EntityManager em = emf.createEntityManager();
         Query q = em.createNamedQuery("Cars.findById");
-        q.setParameter("id",id);
+        q.setParameter("id", id);
         Cars car = (Cars) q.getResultList().get(0);
         return car;
     }
-    public Collection<Cars> getByName(String name){
+
+    public Collection<Cars> getByName(String name) {
         EntityManager em = emf.createEntityManager();
         Query q = em.createNamedQuery("");
         q.setParameter("name", name);
         Collection<Cars> carList = q.getResultList();
         return carList;
-    } 
-    public Collection<Cars> getByDistDrivenMAX(int dist){
+    }
+
+    public Collection<Cars> getByDistDrivenMAX(int dist) {
         EntityManager em = emf.createEntityManager();
-        Query q = em.createNamedQuery("");
+        Query q = em.createNamedQuery("Cars.findByDistDrivenMAX");
         Collection<Cars> carList = q.getResultList();
         return carList;
-    } 
+    }
+
+    public Collection<Cars> getByDistDrivenMIN(int dist) {
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createNamedQuery("Cars.findByDistDrivenMIN");
+        Collection<Cars> carList = q.getResultList();
+        return carList;
+    }
+
 }

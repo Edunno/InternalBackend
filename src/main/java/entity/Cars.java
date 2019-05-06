b6cf09b9ca5b20c62891ef72c4bba7a15bed2e45
Edunno <tamericandream@hotmail.com>
2019-05-06 08:42:41 +0200

    Update Cars.java
    Cars entity edit

--- src/main/java/entity/Cars.java
+++ src/main/java/entity/Cars.java
@@ -8,6 +8,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
@@ -43,6 +45,7 @@ public class Cars implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     @Column(name = "id")
     private Integer id;

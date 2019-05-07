660e9bc9180f574a341000b60841d854e855dca3
Marx02 <casper_marx@msn.com>
2019-05-07 10:32:22 +0200

    DTO updated, authexception added, userfacade added. DTO also takes pass?

---
+++ src/main/java/data/UserFacade.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import entity.Users;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import exceptions.AuthenticationException;
+
+/**
+ *
+ * @author caspe
+ */
+public class UserFacade {
+  
+    private static EntityManagerFactory emf;
+    private static UserFacade instance;
+    
+    private UserFacade(){}
+    
+    public static UserFacade getInstance(EntityManagerFactory factory){
+        if(instance == null){
+          emf = factory;
+          instance = new UserFacade();
+        }
+        return instance;
+    }
+    
+    public Users getVeryfiedUser(String username, String password) throws AuthenticationException {
+        EntityManager em = emf.createEntityManager();
+        Users user;
+        try {
+            user = em.find(Users.class, username);
+            if (user == null || !user.verifyPassword(password)) {
+                throw new AuthenticationException("Invalid user name or password");
+            }
+        } finally {
+            em.close();
+        }
+        return user;
+    }
+
+}
--- src/main/java/dto/UsersDTO.java
+++ src/main/java/dto/UsersDTO.java
@@ -3,8 +3,10 @@
 package dto;
 
 import entity.Rentals;
+import entity.Role;
 import entity.Users;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Esben
@@ -13,6 +15,7 @@ import java.util.Collection;
 public class UsersDTO {
     
     int id;
+    String userName;
     String fullName;
     String email;
     String gender;
@@ -21,9 +24,11 @@ public class UsersDTO {
     int longitude;
     int latitude;
     Collection<Rentals> rentalsCollcetion;
+    List<Role> role;
     
     public UsersDTO(Users a){
         this.id = a.getId();
+        this.userName = a.getUserName();
         this.fullName = a.getFullName();
         this.email = a.getEmail();
         this.gender = a.getGender();
